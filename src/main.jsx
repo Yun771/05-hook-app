983e1a654b70915756398bbdc439aa02e0e78a08
emartinez <ever.martinez@grupotum.com>
2023-03-01 18:03:30 -0600

    use reducer v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Padre } from './07-tarea-memo/Padre'
+
+import './08-reducer/intro-reducer'
+
+// import { Padre } from './07-tarea-memo/Padre'
 // import { CallbackHook } from './06-memos/CallbackHook'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { Memorize } from './06-memos/Memorize'
@@ -17,6 +19,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Padre />
+  // <Padre />
   // </React.StrictMode>,
 )
